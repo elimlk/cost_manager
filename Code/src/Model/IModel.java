57d4a881bd7a223d996c1f65a37e90ed937fd40d
elimlk <48507321+elimlk@users.noreply.github.com>
2020-12-15 14:23:09 +0200

    start working with derbyDB

--- Code/src/Model/IModel.java
+++ Code/src/Model/IModel.java
@@ -3,4 +3,5 @@ package Model;
 public interface IModel {
     public void addCostItem(CostItem item) throws CostManagerException;
     public void deleteCostItem(CostItem item) throws CostManagerException;
+
 }
