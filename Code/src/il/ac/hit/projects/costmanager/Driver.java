46311428a4aa960f3e1bb0ae3388a8c22a5aaf34
elimlk <48507321+elimlk@users.noreply.github.com>
2021-02-07 14:30:59 +0200

    final push

--- Code/src/il/ac/hit/projects/costmanager/Driver.java
+++ Code/src/il/ac/hit/projects/costmanager/Driver.java
@@ -12,21 +12,7 @@ public class  Driver {
 
     public static void main(String[] args) {
         // ID | DETAILS | CATEGORY | CURRENCY | DATE | SUM
-/*        DerbyDBModel db = new DerbyDBModel();
-        db.initDB();
-       try {
-            db.addCostItem(new CostItem(20,"test_throw","CaRs", Currency.ILS.toString(),"2000-02-26"));
-            db.addCategory("baby");
-        } catch (CostManagerException e) {
-            System.out.println(e.getMessage());
-        } // add items for tests.
-        db.printTable();
-        try {
-            db.showReport("1991-01-01","1991-02-23");
-            System.out.println("tt");
-        } catch (CostManagerException e) {
-            e.printStackTrace();
-        }*/
+
 
         //creating the application components
         IModel model = new DerbyDBModel();
